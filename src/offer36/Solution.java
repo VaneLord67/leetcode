7c564f4e59e7109db3a1cef27079acf187ee265d
CJR <524429470@qq.com>
2021-11-17 20:03:49 +0800

    feat: offer36.二叉搜索树与双向链表

---
+++ src/offer36/Solution.java
@@ -0,0 +1,27 @@
+package offer36;
+
+/**
+ * @author CJR
+ * @create 2021-11-17-17:23
+ */
+
+public class Solution {
+    Node pre,head;
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+
+    public void dfs(Node cur) {
+        if (cur == null) return;
+        dfs(cur.left);
+        if (pre != null) pre.right = cur;
+        else head = cur;
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
+    }
+}
