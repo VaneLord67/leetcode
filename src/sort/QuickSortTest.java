207018ee95cdcd736396700b442ecd76c6c80ca1
CJR <524429470@qq.com>
2023-02-24 01:27:03 +0800

    doc: QuickSort注释

--- src/sort/QuickSortTest.java
+++ src/sort/QuickSortTest.java
@@ -19,13 +19,13 @@ public class QuickSortTest {
 
     public static int partition(int[] arr, int startIndex, int endIndex) {
         int pivot = startIndex;
-        int left = startIndex;
+        int left = startIndex; // left指向startIndex是为了在{4,5,5,6}这种情况下， 让right能指向4而不是指向5就终止。
         int right = endIndex;
         while (left != right) {
             while(left < right && arr[right] > arr[pivot]) {
                 right--;
             }
-            while(left < right && arr[left] <= arr[pivot]) {
+            while(left < right && arr[left] <= arr[pivot]) {    // 这里的等于是必须的，因为left从startIndex开始，要让left移动起来。
                 left++;
             }
             if (left < right) {
@@ -41,7 +41,7 @@ public class QuickSortTest {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 5, 4, 2, 3};
+        int[] nums = {4,5,5,6};
         quickSort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
